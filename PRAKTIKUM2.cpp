cf944ffec32a707f087efcf2ba068fde4e57ddba
aludpasopati-byte <aludpasopati@gmail.com>
2026-03-03 22:16:38 +0700

    langkah 7 prsedur ,fungsi

--- PRAKTIKUM2.cpp
+++ PRAKTIKUM2.cpp
@@ -39,3 +39,11 @@ using namespace std;
 
 // deklarasi global
 int p, l;
+
+// implementasi prosedur dan fungsi
+void input() {
+    cout << "masukan panjang = ";
+    cin >> p;
+    cout << "masukan lebar = ";
+    cin >> l;
+}
